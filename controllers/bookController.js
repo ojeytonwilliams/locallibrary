222b830dd31e0dc549805224ca12fa909ca8c7fb
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2017-04-28 13:34:54 +0200

    Added book detail stuff and removed the trailing comma in the genre list.

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -54,7 +54,27 @@ exports.book_list = function(req, res, next) {
 
 // Display detail page for a specific book
 exports.book_detail = function(req, res, next) {
-  res.send('NOT IMPLEMENTED: Book detail: ' + req.params.id);
+
+  async.parallel({
+    book: function(callback) {
+
+      Book.findById(req.params.id)
+        .populate('author')
+        .populate('genre')
+        .exec(callback);
+    },
+    book_instance: function(callback) {
+
+      BookInstance.find({ 'book': req.params.id })
+        //.populate('book')
+        .exec(callback);
+    },
+  }, function(err, results) {
+    if (err) { return next(err); }
+    //Successful, so render
+    res.render('book_detail', { title: 'Title', book: results.book, book_instances: results.book_instance } );
+  });
+
 };
 
 // Display book create form on GET
